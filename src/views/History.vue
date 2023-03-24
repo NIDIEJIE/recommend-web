3df2fbf02507d915ae1194d574f5977def3446f7
lin <1430207096@qq.com>
2023-03-24 10:23:07 +0800

    参量输入区域大致完成，参量级联选择器大致完成

--- src/views/History.vue
+++ src/views/History.vue
@@ -5,28 +5,58 @@
       <el-table-column prop="line" label="线路"> </el-table-column>
       <el-table-column label="操作" width="210" align="center">
         <template slot-scope="scope">
-          <el-button type="success">查看 <i class="el-icon-search"></i></el-button>
-          <el-button type="danger">删除 <i class="el-icon-remove-outline"></i></el-button>
+          <el-button type="success" @click="check()">查看 <i class="el-icon-search"></i></el-button>
+
+          <el-button type="danger" @click=confirmDelete()>删除 <i class="el-icon-remove-outline"></i></el-button>
+
         </template>
       </el-table-column>
     </el-table>
     <div style="padding: 10px 0">
-      <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="currentPage4" :page-sizes="[5, 10, 15, 20]" :page-size="10" layout="total, sizes, prev, pager, next, jumper" :total="400"> </el-pagination>
+      <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="currentPage4"
+        :page-sizes="[5, 10, 15, 20]" :page-size="10" layout="total, sizes, prev, pager, next, jumper" :total="400">
+      </el-pagination>
     </div>
+    <el-dialog title="检修方案推荐" :visible.sync="dialogVisibleCheck" width="30%" :before-close="handleClose">
+      <span>{{ text }}</span>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisibleCheck = false">取消</el-button>
+        <el-button type="primary" @click="dialogVisibleCheck = false">导出PDF</el-button>
+      </span>
+    </el-dialog>
+    <el-dialog title="提示" :visible.sync="dialogVisible" width="30%" :before-close="handleClose">
+      <span><i class="el-icon-warning" style="color: red"></i> 确定删除此条历史记录</span>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取消</el-button>
+        <el-button type="primary" @click="dialogVisible = false">确认</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 export default {
   name: 'History',
-  data() {
+  data () {
     const item = {
       time: '2016-05-02 xx-xx-xx',
       line: 'SH-EDG'
     }
     return {
       tableData: Array(8).fill(item),
-      headerBg: 'headerBg'
+      headerBg: 'headerBg',
+      dialogVisible: false,
+      dialogVisibleCheck: false,
+      text: '这是检修方案',
+    }
+  },
+  methods: {
+    check () {
+      this.dialogVisibleCheck = true
+    },
+    // 删除历史记录的提示框
+    confirmDelete () {
+      this.dialogVisible = true;
     }
   }
 }
--- src/views/Recommend.vue
+++ src/views/Recommend.vue
@@ -16,7 +16,8 @@
         </el-form-item>
         <!--参量选择-->
         <el-form-item label="参量">
-          <el-cascader v-model="value" :options="options" @change="handleChange"></el-cascader>
+          <el-cascader v-model="value" :options="options" @change="handleChange"
+            :props="{ multiple: true }"></el-cascader>
         </el-form-item>
         <!--按钮触发事件-->
         <el-form-item>
@@ -34,23 +35,16 @@
     </div>
     <!-- 右侧容器根据左侧所选择的参量显示对应的输入框 -->
     <div class="rightContainer">
-      <img src="../assets/logo.png" alt="" />
-      <img src="../assets/logo.png" alt="" />
-      <img src="../assets/logo.png" alt="" />
-
-      <img src="../assets/logo.png" alt="" />
-      <img src="../assets/logo.png" alt="" />
-
-      <img src="../assets/logo.png" alt="" />
-      <img src="../assets/logo.png" alt="" />
-      <img src="../assets/logo.png" alt="" />
+      <ParamInput></ParamInput>
     </div>
   </div>
 </template>
 
 <script>
 import * as keyParam from "@/assets/data/keyParam";
+import ParamInput from "@/components/ParamInput.vue";
 export default {
+  components: { ParamInput },
   name: 'Recommend',
   data () {
     return {
@@ -73,43 +67,45 @@ export default {
       //级联选择器选项
       // TODO 添加剩余参量选项和value（因为value相同会导致多选问题）
       options: [
+        // 基础
         {
-          value: '',
+          value: 'k1',
           label: keyParam.base[0],
           children: [{
-            value: '',
+            value: 'k1-1',
             label: keyParam.base[1]
           },
           {
-            value: '',
+            value: 'k2-1',
             label: keyParam.base[2]
           },
           {
-            value: '',
+            value: 'k3-1',
             label: keyParam.base[3]
           },
           {
-            value: '',
+            value: 'k4-1',
             label: keyParam.base[4]
           },
           {
-            value: '',
+            value: 'k5-1',
             label: keyParam.base[5]
           },
           {
-            value: '',
+            value: 'k6-1',
             label: keyParam.base[6]
           },
           {
-            value: '',
+            value: 'k7-1',
             label: keyParam.base[7]
           },
           {
-            value: '',
+            value: 'k8-1',
             label: keyParam.base[8]
           }
           ]
         },
+        // 杆塔
         {
           value: '',
           label: keyParam.tower[0],
@@ -134,7 +130,167 @@ export default {
             label: keyParam.tower[5]
           },
           ]
-        }
+        },
+        // 导地线
+        {
+          value: '',
+          label: keyParam.groundConductor[0],
+          children: [{
+            value: '',
+            label: keyParam.groundConductor[1]
+          },
+          {
+            value: '',
+            label: keyParam.groundConductor[2]
+          },
+          {
+            value: '',
+            label: keyParam.groundConductor[3]
+          },
+          {
+            value: '',
+            label: keyParam.groundConductor[4]
+          },
+          {
+            value: '',
+            label: keyParam.groundConductor[5]
+          },
+          ]
+        },
+        // 绝缘子
+        {
+          value: '',
+          label: keyParam.insulator[0],
+          children: [{
+            value: '',
+            label: keyParam.insulator[1]
+          },
+          {
+            value: '',
+            label: keyParam.insulator[2]
+          },
+          {
+            value: '',
+            label: keyParam.insulator[3]
+          },
+          {
+            value: '',
+            label: keyParam.insulator[4]
+          },
+          {
+            value: '',
+            label: keyParam.insulator[5]
+          },
+          {
+            value: '',
+            label: keyParam.insulator[5]
+          },
+          {
+            value: '',
+            label: keyParam.insulator[5]
+          },
+          {
+            value: '',
+            label: keyParam.insulator[6]
+          },
+          {
+            value: '',
+            label: keyParam.insulator[7]
+          },
+          {
+            value: '',
+            label: keyParam.insulator[8]
+          },
+          {
+            value: '',
+            label: keyParam.insulator[9]
+          },
+          {
+            value: '',
+            label: keyParam.insulator[10]
+          },
+          ]
+        },
+        // 金具
+        {
+          value: '',
+          label: keyParam.metalFittings[0],
+          children: [{
+            value: '',
+            label: keyParam.metalFittings[1]
+          },
+          {
+            value: '',
+            label: keyParam.metalFittings[2]
+          },
+          {
+            value: '',
+            label: keyParam.metalFittings[3]
+          },
+          {
+            value: '',
+            label: keyParam.metalFittings[4]
+          },
+          {
+            value: '',
+            label: keyParam.metalFittings[5]
+          },
+          {
+            value: '',
+            label: keyParam.metalFittings[6]
+          },
+          ]
+        },
+        // 接地装置
+        {
+          value: '',
+          label: keyParam.groundingDevice[0],
+          children: [{
+            value: '',
+            label: keyParam.groundingDevice[1]
+          },
+          {
+            value: '',
+            label: keyParam.groundingDevice[2]
+          }
+          ]
+        },
+        // 通道环境
+        {
+          value: '',
+          label: keyParam.channelEnvironment[0],
+          children: [{
+            value: '',
+            label: keyParam.channelEnvironment[1]
+          },
+          ]
+        },
+        // 附属设施
+        {
+          value: '',
+          label: keyParam.ancillaryFacilities[0],
+          children: [{
+            value: '',
+            label: keyParam.ancillaryFacilities[1]
+          },
+          {
+            value: '',
+            label: keyParam.ancillaryFacilities[2]
+          },
+          {
+            value: '',
+            label: keyParam.ancillaryFacilities[3]
+          },
+          {
+            value: '',
+            label: keyParam.ancillaryFacilities[4]
+          },
+          {
+            value: '',
+            label: keyParam.ancillaryFacilities[5]
+          },
+          ]
+        },
       ]
     }
   },
